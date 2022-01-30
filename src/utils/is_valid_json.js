45034e55184c066c9d494a781775b5454fac79b6
Esinko <34211401+Esinko@users.noreply.github.com>
2022-01-30 23:09:38 +0200

    fix: Remove extra "finally", do not handle objects and arrays gracefully

--- src/utils/is_valid_json.js
+++ src/utils/is_valid_json.js
@@ -4,16 +4,12 @@
  * @returns {boolean}
  */
 module.exports = function (value) {
-    if (typeof value !== "object" && !Array.isArray(value)) value = value.toString() // This should work on all sorts of buffers. Objects and such will break.
-    else return false
+    value = value.toString() // This should work on all sorts of buffers. Objects and such will break.
     try {
         JSON.parse(value)
         return true
     }
     catch(e) {
-        return false
-    }
-    finally {
         return false
     }
 }
