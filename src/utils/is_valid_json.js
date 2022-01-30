a11cc439d5fc84835d0c4f626691b18a9e269ca2
Esinko <34211401+Esinko@users.noreply.github.com>
2022-01-30 23:01:53 +0200

    fix: Consider empty JSON as valid

--- src/utils/is_valid_json.js
+++ src/utils/is_valid_json.js
@@ -1,7 +1,5 @@
 /**
  * Check if something is valid json.
- * 
- * Empty JSON is also considered invalid.
  * @param {*} value The variable to test
  * @returns {boolean}
  */
@@ -9,8 +7,8 @@ module.exports = function (value) {
     if (typeof value !== "object" && !Array.isArray(value)) value = value.toString() // This should work on all sorts of buffers. Objects and such will break.
     else return false
     try {
-        const possiblyJson = JSON.parse(value)
-        if (Object.keys(possiblyJson).length > 0) return true // Empty JSON is considered invalid JSON. Why would you want to parse an empty object?
+        JSON.parse(value)
+        return true
     }
     catch(e) {
         return false
