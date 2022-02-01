72d3649a777001e1e65c3f902df85100998d1d2d
Esinko <34211401+Esinko@users.noreply.github.com>
2022-02-01 12:01:20 +0200

    feat: Display performance information in /info

--- src/modules/info.js
+++ src/modules/info.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 const { Interaction } = require("discord.js")
 const { createHash } = require("crypto")
+const { performance } = require("perf_hooks")
 const findMessage = require("../utils/find_message")
 const checkForAccess = require("../utils/check_for_access")
 const PatchedMessageEmbed = require("../utils/message_embed_patch")
@@ -24,7 +25,6 @@ module.exports = async (interaction, next) => {
         })
 
         let canTimeout = true
-
         setTimeout(() => {
             if (!canTimeout) return
             interaction.followUp({
@@ -33,10 +33,21 @@ module.exports = async (interaction, next) => {
             })
         }, 5000)
 
+        const atExecution = performance.now()
+
         const roleSelection = await global.schemas.RoleSelectionModel.find({ id: interaction.guild.id }).exec()
         const polls = await global.schemas.PollModel.find({ id: interaction.guild.id }).exec()
         let ctfdIntegration = await global.schemas.CTFdIntegrationModel.findOne({ id: interaction.guild.id }).exec()
         if (ctfdIntegration === null) ctfdIntegration = {}
+
+        const atQueryComplete = performance.now()
+        const queryTimeTaken = atQueryComplete - atExecution
+
+        const roleSelectionData = roleSelection.length !== 0 ? (await Promise.all(roleSelection.map(async (document) => (await findMessage(document.message, interaction.guild))?.url ?? "invalid-message"))).join(", ") : "none"
+        const pollData = polls.length !== 0 ? (await Promise.all(polls.map(async (document) => (await findMessage(document.message, interaction.guild))?.url ?? "invalid-message"))).join(" ") : "none"
+
+        const afterMessageQuery = performance.now()
+        const messageQueryTimeTaken = afterMessageQuery - atQueryComplete
 
         const embed = new PatchedMessageEmbed()
             .setAuthor("Server configuration")
@@ -44,16 +55,24 @@ module.exports = async (interaction, next) => {
             .setColor("#667bc4")
             .setThumbnail(interaction.guild.iconURL())
             .addField("Role selection(s)", `
-                Messages: ${roleSelection.length !== 0 ? (await Promise.all(roleSelection.map(async (document) => (await findMessage(document.message, interaction.guild))?.url ?? "invalid-message"))).join(", ") : "none"}
+                Messages: ${roleSelectionData}
             `)
             .addField("Poll(s)", `
-                Messages: ${polls.length !== 0 ? (await Promise.all(polls.map(async (document) => (await findMessage(document.message, interaction.guild))?.url ?? "invalid-message"))).join(" ") : "none"}
+                Messages: ${pollData}
             `)
             .addField("CTFd integration", `
                 CTFd API URL: \`${ctfdIntegration.apiUrl ?? "none"}\`
                 CTFd API Token SHA256 hash: \`${ctfdIntegration.apiToken !== null && ctfdIntegration.apiToken !== undefined ? createHash("sha256").update(ctfdIntegration.apiToken).digest("hex") : "none"}\`
                 CTFd challenge notification channel: <#${ctfdIntegration.challengeNotifications ?? "none"}>
                 CTFd solve notification channel: <#${ctfdIntegration.solveNotifications ?? "none"}>
+            `)
+            .addField("Performance", `
+                \`\`\`
+                Ping:               ${Math.round(global.client.ws.ping)} ms
+                Database:           ${Math.round(queryTimeTaken)} ms
+                Message queries:    ${Math.round(messageQueryTimeTaken)} ms
+                \`\`\`\n
+                Total:              ~${afterMessageQuery - atExecution + global.client.ws.ping} ms
             `)
         canTimeout = false
         interaction.followUp({
